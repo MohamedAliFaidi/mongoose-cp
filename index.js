22e1075968582f4acc6e880e1f76cdf28cb1b61f
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-30 17:40:45 +0100

    cp added

---
+++ index.js
@@ -0,0 +1,144 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+console.log(process.env.DATA_BASE_URL);
+
+/// connect mongodb to express app
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DATA_BASE_URL)
+  .then((res) => console.log("database connected"))
+  .catch((err) => console.log(err));
+
+// define Person Model
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  age: Number,
+  favoriteFoods: [String],
+});
+const Person = mongoose.model("Person", personSchema);
+
+// create Person Route
+
+app.post("/create_person", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newPerson = new Person(req.body);
+    await newPerson.save();
+    res.status(200).json({ message: "success", data: newPerson });
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
+
+// Create Many Persons
+
+app.post("/create_many", async (req, res) => {
+  try {
+    console.log(req.body);
+    const persons = await Person.create(req.body);
+
+    res.status(200).json({ message: "success", data: persons });
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
+
+// find a person by name
+
+app.get("/find_by_name/:name", async (req, res) => {
+  try {
+    console.log(req.params);
+    const found = await Person.find({ name: req.params.name });
+    // const person = await Person.findOne({ favoriteFood: { $in: [food] } });
+
+    res.status(200).json({ message: "success", data: found });
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
+
+// find by food
+app.get("/find_by_food", async (req, res) => {
+  try {
+    console.log(req.query);
+
+    const person = await Person.findOne({
+      favoriteFoods: { $in: [req.query.food] },
+    });
+
+    res.status(200).json({ message: "success", data: person });
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
+});
+
+
+
+// find personby id 
+app.get("/find_by_id", async (req, res) => {
+    try {
+      console.log(req.query);
+  
+      const person = await Person.findById(req.query.id)
+    
+  
+      res.status(200).json({ message: "success", data: person });
+    } catch (error) {
+      res.status(500).json({ message: "error" });
+    }
+  });
+
+
+  // find by id and update food (classic)
+
+  app.post("/find_by_id_and_update", async (req, res) => {
+    try {
+      console.log(req.query);
+  
+      const person = await Person.findById(req.query.id)
+
+      person.favoriteFoods.push(req.body.food)
+      await person.save()
+    
+  
+      res.status(200).json({ message: "success", data: person });
+    } catch (error) {
+      res.status(500).json({ message: "error" });
+    }
+  });
+
+// modern update
+  app.post("/find_by_name_and_update/:name", async (req, res) => {
+    try {
+      console.log(req.params);
+
+    //   const updated = await Person.findOneAndUpdate({name:req.params.name} , {
+    //      $push: { favoriteFoods: req.body.food }
+
+    //   } ,{new:true})
+        const updated = await Person.findOneAndUpdate({name:req.params.name} , {
+         age: req.body.age
+
+      } ,{new:true})
+  
+      res.status(200).json({ message: "success", data: updated });
+    } catch (error) {
+      res.status(500).json({ message: "error" });
+    }
+  });
+  
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
